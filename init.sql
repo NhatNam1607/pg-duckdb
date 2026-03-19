b8bb4550226d5e122eb63d7250ba02c4dccb8821
NhatNam067 <nhatnam160701@gmail.com>
2026-03-19 22:29:34 +0700

    Fix init.sql error

--- init.sql
+++ init.sql
@@ -1,10 +1,9 @@
-
 CREATE EXTENSION IF NOT EXISTS pg_duckdb;
 
-CREATE TABLE IF NOT EXISTS demo_analytics_events (
+
+CREATE TABLE IF NOT EXISTS demo_analytics_events_pg (
     id SERIAL,
     event_type VARCHAR,
     user_id INT,
     event_time TIMESTAMP
-) USING duckdb;
-
+);
